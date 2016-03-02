aeb5f80d559882f80633628ecfd849862bf25c1c
vpippal2 <vpippal@centrum.cz>
2016-03-02 13:33:59 +0100

    tests with HTML form method:PUT

--- Inventory.Web/Services/RestStore.cs
+++ Inventory.Web/Services/RestStore.cs
@@ -1,17 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using Inventory.Messaging;
+//using Inventory.Persistence.Engine;
+//using Inventory.Persistence.Models;
+//using Biggy.Data.Json;
 
 namespace Inventory.Web.Services
 {
-  public class RestStore:IStore
-  {
-    public void SaveEvents(Guid aggregateId, IEnumerable<Event> events, int expectedVersion)
+    public class RestStore : IStore
     {
-      throw new NotImplementedException();
-    }
+        public RestStore()
+        {
+            //TODO: use persistent store ????
+            //_persistentStore = new Store(new JsonStore<Persistence.Models.EventDescriptor>(".", "webstaore", "events"), new JsonSerializer());
+        }
 
-    public List<Event> GetEventsForAggregate(Guid aggregateId)
+        //private Persistence.Engine.Store _persistentStore;
+
+        public void SaveEvents(Guid aggregateId, IEnumerable<Event> events, int expectedVersion)
+        {
+            //throw new NotImplementedException();
+            Console.WriteLine("store id:" + aggregateId);
+            //TODO: use persistent store ????
+            //_persistentStore.SaveEvents(aggregateId, events, expectedVersion);
+        }
+
+        public List<Event> GetEventsForAggregate(Guid aggregateId)
     {
       throw new NotImplementedException();
     }
