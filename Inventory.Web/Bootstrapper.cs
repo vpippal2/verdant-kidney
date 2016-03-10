3854e421f380d1bb4029817523ae45460342bbdd
vpippal2 <vpippal@centrum.cz>
2016-03-10 01:54:10 +0100

    Bootstrapper inherits from PersistenceBootstrapper (allows usage of Inventory.Persistence)

--- Inventory.Web/Bootstrapper.cs
+++ Inventory.Web/Bootstrapper.cs
@@ -1,13 +1,17 @@
 ﻿using Inventory.Handlers;
 using Inventory.Messaging;
+using Inventory.Persistence;
 using Inventory.Web.Services;
 using Nancy;
+using Nancy.Bootstrapper;
 
 namespace Inventory.Web
 {
-  public class Bootstrapper:DefaultNancyBootstrapper
-  {
-    protected override void ApplicationStartup(Nancy.TinyIoc.TinyIoCContainer container, Nancy.Bootstrapper.IPipelines pipelines)
+    // Set PersistenceBootstrapper as parrent to allow usage of "Inventory.Persistence" in Web project
+    // NOTE: the object is created automaticaly (inherited is created while parent is not)
+    public class Bootstrapper: PersistenceBootstrapper //DefaultNancyBootstrapper
+    {
+      protected override void ApplicationStartup(Nancy.TinyIoc.TinyIoCContainer container, Nancy.Bootstrapper.IPipelines pipelines)
     {
       Wiring.Wire();
       base.ApplicationStartup(container, pipelines);
