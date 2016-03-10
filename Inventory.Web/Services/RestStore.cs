ef2b4a3d6b41a7e77a304c9eb9b90b73fcaa0c91
vpippal2 <vpippal@centrum.cz>
2016-03-10 01:49:33 +0100

    use Store from Inventory.Persistence

--- Inventory.Web/Services/RestStore.cs
+++ Inventory.Web/Services/RestStore.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using Biggy.Data.Json;
 using Inventory.Messaging;
+using Inventory.Persistence.Engine;
+
 //using Inventory.Persistence.Engine;
 //using Inventory.Persistence.Models;
 //using Biggy.Data.Json;
@@ -9,10 +12,12 @@ namespace Inventory.Web.Services
 {
     public class RestStore : IStore
     {
+        private Store _persistentStore;
+
         public RestStore()
         {
             //TODO: use persistent store ????
-            //_persistentStore = new Store(new JsonStore<Persistence.Models.EventDescriptor>(".", "webstaore", "events"), new JsonSerializer());
+            _persistentStore = new Store(new JsonStore<Persistence.Models.EventDescriptor>(@"c:\temp\_a_", "webstore", "events"), new JsonSerializer());
         }
 
         //private Persistence.Engine.Store _persistentStore;
@@ -20,14 +25,15 @@ namespace Inventory.Web.Services
         public void SaveEvents(Guid aggregateId, IEnumerable<Event> events, int expectedVersion)
         {
             //throw new NotImplementedException();
-            Console.WriteLine("store id:" + aggregateId);
             //TODO: use persistent store ????
-            //_persistentStore.SaveEvents(aggregateId, events, expectedVersion);
+            _persistentStore.SaveEvents(aggregateId, events, expectedVersion);
         }
 
         public List<Event> GetEventsForAggregate(Guid aggregateId)
-    {
-      throw new NotImplementedException();
+        {
+            //throw new NotImplementedException();
+            //TODO: use persistent store ????
+            return _persistentStore.GetEventsForAggregate(aggregateId);
+        }
     }
-  }
 }
